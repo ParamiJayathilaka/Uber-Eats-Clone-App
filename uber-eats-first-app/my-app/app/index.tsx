dcaa3350e95b66f4986dc1c018e8b786936a39a0
ParamiJayathilaka <thinithiparami53@gmail.com>
2024-07-11 14:46:21 +0530

    login done

--- uber-eats-first-app/my-app/app/index.tsx
+++ uber-eats-first-app/my-app/app/index.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
+import { Text, View, StyleSheet, Image, Button, Alert } from 'react-native';
 
 export default function Index() {
   return (
     <View style={styles.backgroundView}>
-      <View style={styles.textContainer}>
-        <Text style={styles.title}>Uber</Text>
-        <Text style={styles.subtitle}>Eats</Text>
-      </View>
       <Image
         style={styles.tinyLogo}
-        source={require('../assets/images/login.jpg')}
+        source={require('../assets/images/login1.jpg')}
       />
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>Uber Eats</Text>
+      </View>
+      <Text style={styles.title1}>
+        Get Started With Uber Eats
+      </Text>
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Continue "
+          color="#02520b"
+          onPress={() => Alert.alert('Simple Button pressed')}
+        />
+      </View>
     </View>
   );
 }
@@ -20,23 +29,38 @@ const styles = StyleSheet.create({
   backgroundView: {
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: 'white',
   },
+  tinyLogo: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+  },
   textContainer: {
-    marginTop: 20,
+    position: 'absolute',
+    top: '5%', 
     alignItems: 'center',
   },
-  tinyLogo: {
-    width: 400,
-    height: 400,
-    marginTop: 20,
-  },
   title: {
-    fontSize: 30,
+    fontSize: 40, 
     fontWeight: 'bold',
+    color: 'black', 
+  },
+  title1: {
+    textAlign: 'center',
+    marginVertical: 8,
+    position: 'absolute',
+    top: '80%', 
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 20, 
+    alignSelf: 'center',
+    width: 300,
     
   },
-  subtitle: {
-    fontSize: 24,
-  },
 });
+
